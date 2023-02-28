473130bea4065788ce1e7cb9f092fb6d7a6941d1
jasine <jasinechen@gmail.com>
2023-02-28 21:49:59 +0800

    fix: len
    Signed-off-by: jasine <jasinechen@gmail.com>

--- pkg/hub/localizer/localizer.go
+++ pkg/hub/localizer/localizer.go
@@ -311,7 +311,7 @@ func (l *Localizer) ApplyOverridesToDescription(desc *appsapi.Description) error
 				continue
 			}
 
-			klog.V(1).Infof("====== overrides len: ", len(overrides))
+			klog.V(1).Infof("====== overrides len: %d", len(overrides))
 
 			// use a whitespace explicitly
 			genericResult, chartOverrideResult, err := applyOverrides([]byte(" "), []byte(" "), overrides)
